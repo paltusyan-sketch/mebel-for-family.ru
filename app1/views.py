891f450cf81c8813126613ef0756a90931b096e8
Vitaly <paltusyan@gmail.com>
2026-02-28 16:16:37 +0700

    Направил весь СПАМ в отдельную группу, где смогу отследить на какую ловушку ведутся боты

--- app1/views.py
+++ app1/views.py
@@ -9,18 +9,24 @@ from random import choice
 
 # Create your views here.
 
-def send_telegram_notification(data, is_spam=False):
+def send_telegram_notification(data, is_spam=False, chat_id=settings.SETTING_TELEGRAM_CHAT_ID):
     if data['comment']:
         comment = f"\nКомментарий: <b>{data['comment']}</b>"
     else:
         comment = ""
+    
+    if is_spam:
+        spam_message = f"СПАМ !!!\n\n<b>imail:</b> {data['imail'] if data['imail'] else 'None'}\n<b>honeypot:</b> {data['honeypot'] if data['honeypot'] else 'None'}\n\n"
+    else:
+        spam_message = ""
+
 
     raw_phone = data['phone'].replace(' ', '').replace('(', '').replace(')', '').replace('-', '')
-    message = f"{'СПАМ !!!\n\n' if is_spam else ""}Имя: <b>{data['name']}</b>\nТелефон: <b>{raw_phone}</b>" + comment
+    message = spam_message + f"Имя: <b>{data['name']}</b>\nТелефон: <b>{raw_phone}</b>" + comment
 
     url = f"https://api.telegram.org/bot{settings.TELEGRAM_BOT_TOKEN}/sendMessage"
     params = {
-        'chat_id': settings.TELEGRAM_CHAT_ID,
+        'chat_id': chat_id,
         'text': message,
         'parse_mode': 'HTML',
         'disable_notification': is_spam,
@@ -36,11 +42,13 @@ def index_page(request):
         form = OrderForm(request.POST)
         if request.POST.get('imail') or request.POST.get('honeypot'):
             form.is_valid()
-            send_telegram_notification(form.cleaned_data, True)
+            form.cleaned_data['imail'] = request.POST.get('imail')
+            form.cleaned_data['honeypot'] = request.POST.get('honeypot')
+            send_telegram_notification(form.cleaned_data, True, chat_id=settings.SETTING_TELEGRAM_CHAT_ID)
             messages.error(request, 'Система распосзнала, что вы бот!')
             return redirect('main')
         if form.is_valid():
-            send_telegram_notification(form.cleaned_data)
+            send_telegram_notification(form.cleaned_data, False, chat_id=settings.TELEGRAM_CHAT_ID)
             messages.success(request, 'Ваша заявка успешно отправлена!')
             return redirect('main') # Перенаправляем на ту же страницу
     else:
@@ -99,16 +107,18 @@ def contacts_page(request):
         form = OrderForm(request.POST)
         if request.POST.get('imail') or request.POST.get('honeypot'):
             form.is_valid()
-            send_telegram_notification(form.cleaned_data, True)
+            form.cleaned_data['imail'] = request.POST.get('imail')
+            form.cleaned_data['honeypot'] = request.POST.get('honeypot')
+            send_telegram_notification(form.cleaned_data, True, chat_id=settings.SETTING_TELEGRAM_CHAT_ID)
             messages.error(request, 'Система распосзнала, что вы бот!')
             return redirect('contacts')
         if form.is_valid():
-            send_telegram_notification(form.cleaned_data)
+            send_telegram_notification(form.cleaned_data, False, chat_id=settings.TELEGRAM_CHAT_ID)
             messages.success(request, 'Ваша заявка успешно отправлена!')
-            return redirect('contacts') 
+            return redirect('contacts') # Перенаправляем на ту же страницу
     else:
-        # Передаем начальные данные в форму при GET-запросе
-        form = OrderForm(initial=initial_data)
+        # Если метод GET, создаем пустую форму
+        form = OrderForm()
 
     context = {'form': form}
     return render(request, "contacts.html", context)
