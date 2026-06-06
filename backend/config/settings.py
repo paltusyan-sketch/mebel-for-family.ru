67507fa9943664d702f5f09c690667998559d664
Vitaly <paltusyan@gmail.com>
2026-06-06 23:26:28 +0700

    Настройка Docker-инфраструктуры и полный перевод проекта на .env
    - Разделил конфигурацию Docker на локальную и продакшн (docker-compose.prod.yml). - Полностью отказался от local_settings.py, перенёс все секреты в единый текстовый файл .env. - Переписал settings.py (секретный ключ, доступы к PostgreSQL, токены Telegram-ботов) на чтение из переменных окружения через os.environ.get. - Защитил админку на туннеле и продакшене, настроив фильтрацию для CSRF_TRUSTED_ORIGINS и ALLOWED_HOSTS. - Оптимизировал файлы .gitignore и .dockerignore: скрыл секреты, отсёк мусор, сокеты, статику и кэш. - Интегрировал боевой контейнер Nginx для раздачи статических файлов и медиа с защитой read-only.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
@@ -32,16 +33,8 @@ if DEBUG:
 CSRF_TRUSTED_ORIGINS = os.environ.get('DJANGO_CSRF_TRUSTED_ORIGINS', '').split(',')
 CSRF_TRUSTED_ORIGINS = [origin for origin in CSRF_TRUSTED_ORIGINS if origin]
 
-# if os.path.exists('C:\\Users\\'): # Если есть папка Users, значит мы на Windows
-#     ALLOWED_HOSTS = ['*']
-#     CSRF_TRUSTED_ORIGINS = ['https://cognoscitive-kristeen-hypoxic.ngrok-free.dev']
-# else:
-#     ALLOWED_HOSTS = ['uralmeb.ru', 'www.uralmeb.ru', '85.198.102.120', '127.0.0.1', 'localhost',]
-
-
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -90,12 +83,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'), # В докере свяжется по имени сервиса 'db'
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
@@ -145,13 +142,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_collected')
 
 
 
-try:
-    from .local_settings import *
-except ImportError:
-    pass
+TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
+TELEGRAM_CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID')
+SETTING_TELEGRAM_CHAT_ID = os.environ.get('SETTING_TELEGRAM_CHAT_ID')
 
-
-if os.path.exists('C:\\Users\\'): # Если есть папка Users, значит мы на Windows
+if DEBUG:
     TELEGRAM_CHAT_ID = SETTING_TELEGRAM_CHAT_ID
-else:
-    pass
